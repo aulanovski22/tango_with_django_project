ad7f44b2b7c7ce5719f64393bbc1a07b9a776cec
Alexandra Ulanovski <2635364u@student.gla.ac.uk>
2023-01-27 10:55:31 +0000

    chapter 6 done, but still have to do exercises

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rango.models import Page
 from django.http import HttpResponse
 from rango.models import Category
 
@@ -19,3 +19,15 @@ def about(request):
 def about(request):
     context_dict = {'boldmessage': 'The cat was here!'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages']=pages
+        context_dict['category']=category
+    except Category.DoesNotExist:
+        context_dict['category']=None
+        context_dict['pages']=None
+    return render(request, 'rango/category.html', context=context_dict)
