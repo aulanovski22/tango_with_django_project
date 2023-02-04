50b37a77a75a73faa6ad32136164d397ba6d25c7
Alexandra Ulanovski <2635364u@student.gla.ac.uk>
2023-02-04 22:51:00 +0000

    Chapter 9 done

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,11 @@ from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.shortcuts import redirect 
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     #construct a dictionary to pass to the template engine as its context
@@ -18,9 +23,6 @@ def index(request):
     context_dict['page'] = pages_list
     return render(request, 'rango/index.html', context=context_dict)
 
-##def about(request):
-    
-    ##return HttpResponse("It works!")
 
 def about(request):
     about_context_dict = {'boldmessage': 'Rango says here is the about page'}
@@ -30,7 +32,7 @@ def about(request):
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html', {})
-    
+
 def show_category(request, category_name_slug, ):
     context_dict = {}
     try:
@@ -43,21 +45,23 @@ def show_category(request, category_name_slug, ):
         context_dict['pages']=None
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return redirect(reverse('rango: index'))
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
-    except Category.DoesNotExist:
+    except:
         category = None
 
     if category is None:
@@ -78,3 +82,55 @@ def add_page(request, category_name_slug):
             print(form.errors)
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html', context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
